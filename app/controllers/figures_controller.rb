a22037ea1e05b90b8f7d3e7815498a0365659713
Sean Altemose <sean@seanaltemose.com>
2018-07-03 15:29:16 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,22 @@
 class FiguresController < ApplicationController
 
-end 
+    # Read get: Index page
+    get '/figures' do
+      @figures = Figure.all
+
+      erb :'/figures/index'
+    end
+
+    # Read get: New figure page
+    get '/figures/new' do
+      @titles = Title.all.sort_by do |title|
+        title.name
+      end
+      @landmarks = Landmark.all.sort_by do |landmark|
+        landmark.name
+      end
+
+      erb :'/figures/new'
+    end
+
+  end
